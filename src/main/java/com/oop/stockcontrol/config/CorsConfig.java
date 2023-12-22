b88c4bc1a409d7b3b93613f8acce04a848dbc794
lohrenzo <lorenzofblack@gmail.com>
2023-12-22 02:11:24 +0000

    5th Commit (CORS Config)

--- src/main/java/com/oop/stockcontrol/config/CorsConfig.java
+++ src/main/java/com/oop/stockcontrol/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("http://localhost:5173", "https://stockcontrolfrontend-ks6w0v5l4-lohrenzo.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
